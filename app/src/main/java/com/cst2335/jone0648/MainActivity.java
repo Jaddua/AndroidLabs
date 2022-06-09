10100994e115324472f3dc5089433bd29a20537d
jaddua-algonquin <105401765+jaddua-algonquin@users.noreply.github.com>
2022-06-08 20:05:26 -0400

    step 2

--- app/src/main/java/com/cst2335/jone0648/MainActivity.java
+++ app/src/main/java/com/cst2335/jone0648/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //hello
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_constraint);
 
 
 
